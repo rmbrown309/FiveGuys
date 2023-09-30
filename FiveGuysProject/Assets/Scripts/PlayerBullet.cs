3ec0915fde311e2da07e6c6b448394b68be77d5b
JarenUrey <126486342+JarenUrey@users.noreply.github.com>
2023-09-30 15:03:46 -0400

    Player Bullet
    made a game object for player bullet, as well a script

---
+++ FiveGuysProject/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+
+    [Header("----- Bullet Stats -----")]
+    [SerializeField] int damage;
+    [SerializeField] int speed;
+    [SerializeField] int destroyTime;
+
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, destroyTime);
+    }
+
+    //When a bullet collides with something
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+        {
+            return;
+        }
+        /*Need the IDamage script to uncomment following line
+        IDamage damageable = other.GetComponent<IDamage>();
+        if (damageable != null)
+        {
+            damageable.takeDamage(damage);
+        }
+        */
+
+        Destroy(gameObject);
+    }
+}
