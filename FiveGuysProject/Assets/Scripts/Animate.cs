39a6baa62c09a730935c3b9bca129d0de93199c9
Erikson Gomez <119821777+RubberDuky@users.noreply.github.com>
2023-10-09 17:43:59 -0400

    MainMenu
    Animated and added start/ quit button.
    Settings button currently does not lead anywhere

---
+++ FiveGuysProject/Assets/Scripts/Animate.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour
+{
+    private Animator mAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mAnimator != null)
+        {
+            if (Input.GetButtonUp("Shoot"))
+            {
+                mAnimator.SetTrigger("isClicked");
+            }
+        }
+    }
+}
