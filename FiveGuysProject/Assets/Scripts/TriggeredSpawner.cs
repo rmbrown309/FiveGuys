d01d36586138949e36d27e35f54b2434fe008104
rmbrown309 <114186621+rmbrown309@users.noreply.github.com>
2023-11-02 11:40:12 -0400

    Made a triggerable spawner
    New type of enemy spawner that isn't tied to the wave system, and begins spawning when the player enters its trigger.

---
+++ FiveGuysProject/Assets/Scripts/TriggeredSpawner.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggeredSpawner : MonoBehaviour
+{
+    [Header("-----Components------")]
+    [SerializeField] Transform[] posToSpawn;
+    [SerializeField] GameObject[] enemy; // enemy type spawned by this spawner
+    [Header("-----Spawner Stats------")]
+    [SerializeField] int numOfEnemies; // number of enemies spawned during the wave
+    [SerializeField] float spawnRate; // seconds between each emnemy spawn
+    [SerializeField] float spawnRange;
+    [SerializeField] bool rats;
+
+    bool isWaveActive = true;
+    bool isTriggered = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spawns enemies only on the correct wave number
+        if (isTriggered && isWaveActive)
+        {
+            StartCoroutine(TotalEnemy());
+            gameObject.GetComponent<SphereCollider>().enabled = false;
+        }
+    }
+
+    IEnumerator TotalEnemy()
+    {
+        // prevents spawner from starting again during the wave
+        isWaveActive = false;
+
+        Vector3 spawnpoint = posToSpawn[0].position;
+
+        // increments remaining enemies by the amount of enemies about to spawn
+        if (!rats)
+            GameManager.instance.UpdateWinCondition(numOfEnemies);
+
+        // spawns the specified enemies at the specified rate
+        for (int i = 0; i < numOfEnemies; i++)
+        {
+            if (posToSpawn.Length > 1)
+            {
+                Transform furthestSpawn = posToSpawn[0];
+                for (int j = 1; j < posToSpawn.Length; j++)
+                {
+                    // prioritize the spawnpoint furthest from the player in order to keep enemies from spawning on top of them
+                    if ((posToSpawn[j].position - GameManager.instance.player.transform.position).magnitude > (furthestSpawn.position - GameManager.instance.player.transform.position).magnitude)
+                        furthestSpawn = posToSpawn[j];
+                }
+
+                // randomly choose a spawnpoint around the spawner transform in a circle of radius spawnRange
+                spawnpoint = furthestSpawn.position + Random.insideUnitSphere * spawnRange;
+                spawnpoint.y = furthestSpawn.position.y;
+            }
+            else
+            {
+                spawnpoint += Random.insideUnitSphere * spawnRange;
+                spawnpoint.y = posToSpawn[0].position.y;
+            }
+
+            Instantiate(enemy[Random.Range(0, enemy.Length)], spawnpoint, Quaternion.identity);
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isTriggered = true;
+        }
+    }
+}
