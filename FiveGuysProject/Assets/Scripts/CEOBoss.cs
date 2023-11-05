54239ff8418529446393039de994394543b6fe4b
rmbrown309 <114186621+rmbrown309@users.noreply.github.com>
2023-11-05 18:22:46 -0500

    Fixed GAMEBREAKING BUG

--- FiveGuysProject/Assets/Scripts/CEOBoss.cs
+++ FiveGuysProject/Assets/Scripts/CEOBoss.cs
@@ -1,7 +1,6 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class CEOBoss : MonoBehaviour, IDamage, IPhysics
