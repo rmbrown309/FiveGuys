74544368635145ddd3d85f50393db9acb5987394
rmbrown309 <114186621+rmbrown309@users.noreply.github.com>
2023-11-19 14:29:03 -0500

    Disabled quit on title menu and small fix to weird pathing on first level

--- FiveGuysProject/Assets/Scripts/Animate.cs
+++ FiveGuysProject/Assets/Scripts/Animate.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Animate : MonoBehaviour
 {
+
+    [SerializeField] GameObject quitButton;
+
     private Animator mAnimator;
     // Start is called before the first frame update
     void Start()
     {
         mAnimator = GetComponent<Animator>();
+
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+            Destroy(quitButton);
     }
 
     // Update is called once per frame
